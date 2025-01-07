ccfbd73cd74295024c0b3b5e2971a1eb04e0df78
Anil Vishwakarma <anil.vishwakarma.it@gmail.com>
2025-01-07 05:31:32 +0000

    1st program in c on github codespace with exptension

---
+++ p1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// #include<conio.h>
+int main()
+{
+  printf("1. Hello world\n ");
+  printf("2. Hello world\n ");
+  printf("3. Hello world\n ");
+  printf("Hello world\n ");
+  printf("Hello world\n ");
+  printf("Hello world\n ");
+  printf("7. Hello world");
+  for (int i = 0; i < 266; i++)
+  {
+    /* code */
+    printf("%d\n", i);
+
+  }
+  
+  
+}
